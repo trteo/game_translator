3483c583c650ad2e512d7fdeab34a160365b48c6
f.tropin <100.ac@mail.ru>
2024-05-03 22:20:18 +0300

    refactor: put all language to enums, add mapping between them

--- models/languges.py
+++ models/languges.py
@@ -2,17 +2,44 @@ from enum import Enum
 from typing import Dict
 
 
-class DeeplSourceLangs(Enum):
-    ENGLISH = 'EN'
-    RUSSIAN = 'RU'
-
-
-class FileSourceLangs(Enum):
-    ENGLISH = 'eng'
+class SourceLangsCodes(Enum):
     RUSSIAN = 'rus'
+    ENGLISH = 'eng'
+    UKRAINE = 'ukr'
+    SPANISH = 'spa'
+    PORTUGUESE_BRAZILIAN = 'pbr'
+
+    GERMAN = 'deu'
+    ITALIAN = 'ita'
+    TURKISH = 'tur'
 
 
-SOURCE_FILE_2_DEEPL_MAP: Dict[FileSourceLangs, DeeplSourceLangs] = {
-    FileSourceLangs.ENGLISH: DeeplSourceLangs.ENGLISH,
-    FileSourceLangs.RUSSIAN: DeeplSourceLangs.RUSSIAN,
+class DeeplLangsCodes(Enum):
+    RUSSIAN = "ru"
+    ENGLISH = "en-us"
+    UKRAINE = "uk"
+    SPANISH = "es"
+    PORTUGUESE_BRAZILIAN = "pt-br"
+
+    GERMAN = "de"
+    ITALIAN = "it"
+    TURKISH = "tr"
+
+
+SOURCE_FILE_2_DEEPL_MAP: Dict[SourceLangsCodes, str] = {
+    SourceLangsCodes.RUSSIAN: DeeplLangsCodes.RUSSIAN.value,
+    SourceLangsCodes.ENGLISH: DeeplLangsCodes.ENGLISH.value,
+}
+
+
+DEEPL_2_SOURCE_FILE_MAP: Dict[str, str] = {
+    DeeplLangsCodes.RUSSIAN.value: SourceLangsCodes.RUSSIAN.value,
+    DeeplLangsCodes.ENGLISH.value: SourceLangsCodes.ENGLISH.value,
+    DeeplLangsCodes.UKRAINE.value: SourceLangsCodes.UKRAINE.value,
+    DeeplLangsCodes.SPANISH.value: SourceLangsCodes.SPANISH.value,
+    DeeplLangsCodes.PORTUGUESE_BRAZILIAN.value: SourceLangsCodes.PORTUGUESE_BRAZILIAN.value,
+
+    DeeplLangsCodes.GERMAN.value: SourceLangsCodes.GERMAN.value,
+    DeeplLangsCodes.ITALIAN.value: SourceLangsCodes.ITALIAN.value,
+    DeeplLangsCodes.TURKISH.value: SourceLangsCodes.TURKISH.value,
 }
--- src/translatpr.py
+++ src/translatpr.py
@@ -4,7 +4,7 @@ import requests
 import toml
 from typing import Dict
 
-from models.languges import SOURCE_FILE_2_DEEPL_MAP, FileSourceLangs
+from models.languges import SOURCE_FILE_2_DEEPL_MAP, SourceLangsCodes, DEEPL_2_SOURCE_FILE_MAP
 from settings.config import settings, BASE_DIR
 
 
@@ -12,13 +12,13 @@ SOURCE_DIR = BASE_DIR / 'data' / 'source'
 RESULT_DIR = BASE_DIR / 'data' / 'result'
 
 
-def translate_text(text: str, source_file_lang: FileSourceLangs, target_lang: str) -> str:
+def translate_text(text: str, source_file_lang: SourceLangsCodes, target_lang: str) -> str:
     """Translate text from source language to target language using DeepL API."""
     url = "https://api-free.deepl.com/v2/translate"
     params = {
         "auth_key": settings.DEEPL_API_KEY,
         "text": text,
-        "source_lang": SOURCE_FILE_2_DEEPL_MAP.get(source_file_lang).value,
+        "source_lang": SOURCE_FILE_2_DEEPL_MAP.get(source_file_lang),
         "target_lang": target_lang
     }
     response = requests.post(url, data=params)
@@ -32,7 +32,7 @@ def translate_and_save_to_toml(
     languages: Dict[str, str],
     input_file_path: Path,
     output_file_path: Path,
-    source_lang_code: FileSourceLangs
+    source_lang_code: SourceLangsCodes
 ) -> None:
     """Load data from TOML, translate and save to another TOML file."""
     # Load original data
@@ -44,7 +44,11 @@ def translate_and_save_to_toml(
         translations[key] = {}
         for lang_code, lang_name in languages.items():
             # Translate each term using the source language provided
-            translations[key][lang_code.lower()] = translate_text(values[source_lang_code.value], source_lang_code, lang_code)
+            translations[key][lang_code.lower()] = translate_text(
+                text=values[source_lang_code.value],
+                source_file_lang=source_lang_code,
+                target_lang=lang_code
+            )
 
     # Save translations to new TOML file
     with output_file_path.open("w", encoding="utf-8") as toml_file:
@@ -52,25 +56,15 @@ def translate_and_save_to_toml(
 
 
 if __name__ == "__main__":
-    languages: Dict[str, str] = {
-        "EN-US": "eng",
-        "UK": "ukr",
-        "ES": "spa",
-        "PT-BR": "pbr",
-        "DE": "deu",
-        "IT": "ita",
-        "TR": "tur"
-    }
-
     input_file_path = SOURCE_DIR / "cleanup.toml"
     output_file_path = RESULT_DIR / "translations.toml"
 
     # Choose the source language code from the original data file
-    source_lang_code = FileSourceLangs.RUSSIAN
+    source_lang_code = SourceLangsCodes.RUSSIAN
 
     # Translate and save to TOML file
     translate_and_save_to_toml(
-        languages=languages,
+        languages=DEEPL_2_SOURCE_FILE_MAP,
         input_file_path=input_file_path,
         output_file_path=output_file_path,
         source_lang_code=source_lang_code
