d07a0b3b05ce6b12b59999ac010971f331e0756d
f.tropin <100.ac@mail.ru>
2024-05-03 17:21:40 +0300

    refactor: fix params splitting

--- src/translatpr.py
+++ src/translatpr.py
@@ -1,8 +1,12 @@
 import requests
 import toml
 from typing import Dict
+from settings.config import settings, BASE_DIR
 
-from settings.config import settings
+
+SOURCE_DIR = BASE_DIR / 'data' / 'source'
+RESULT_DIR = BASE_DIR / 'data' / 'result'
+
 
 def translate_text(text: str, target_lang: str) -> str:
     url = "https://api-free.deepl.com/v2/translate"
@@ -17,27 +21,31 @@ def translate_text(text: str, target_lang: str) -> str:
     else:
         return f"Translation failed with status code {response.status_code}"
 
-def translate_and_save_to_toml(data: Dict[str, str], languages: Dict[str, str], file_path: str) -> None:
+
+def load_data_from_toml(file_path: str) -> Dict[str, str]:
+    with open(file_path, "r", encoding="utf-8") as toml_file:
+        return toml.load(toml_file)
+
+
+def translate_and_save_to_toml(
+    languages: Dict[str, str],
+    input_file_path: str,
+    output_file_path: str
+) -> None:
+    data_to_translate = load_data_from_toml(file_path=input_file_path)
+
     translations = {}
-    for key, value in data.items():
+    for key, value in data_to_translate.items():
         translations[key] = {}
         for lang_code, lang_name in languages.items():
-            translations[key][lang_code.lower()] = translate_text(value, lang_code)
+            translated_text = translate_text(text=value, target_lang=lang_code)
+            translations[key][lang_code.lower()] = translated_text
 
-    with open(file_path, "a", encoding="utf-8") as toml_file:
+    with open(output_file_path, "w", encoding="utf-8") as toml_file:
         toml.dump(translations, toml_file)
 
-if __name__ == "__main__":
-    # Define your data
-    data: Dict[str, str] = {
-        "apple": "ROTTING APPLE",
-        "banana": "BANANA PEEL",
-        "chicken": "GNAWED CHICKEN",
-        "fish": "FISH BONES",
-        "orange": "ORANGE PEEL"
-    }
 
-    # Define languages
+if __name__ == "__main__":
     languages: Dict[str, str] = {
         "EN-US": "eng",
         "UK": "ukr",
@@ -48,9 +56,11 @@ if __name__ == "__main__":
         "TR": "tur"
     }
 
-    # Translate and save to TOML file
+    input_file_path = SOURCE_DIR / "cleanup.toml"
+    output_file_path = RESULT_DIR / "translations.toml"
+
     translate_and_save_to_toml(
-        data,
-        languages,
-        "C:\\Users\\f.tropin\\Documents\\work\\perevodichik_ebeyshey_igry\\data\\result\\translations.toml"
+        languages=languages,
+        input_file_path=input_file_path,
+        output_file_path=output_file_path
     )
