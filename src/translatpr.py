b854c3c5b139afc52796d95879b91e2d9476271e
f.tropin <100.ac@mail.ru>
2024-05-03 23:53:15 +0300

    refactor: addd logging

--- src/translatpr.py
+++ src/translatpr.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import requests
 import toml
 from typing import Dict
+from loguru import logger
 
 from models.languages import (
     SOURCE_FILE_2_DEEPL_MAP,
@@ -10,6 +11,8 @@ from models.languages import (
 )
 from settings.config import settings, BASE_DIR
 
+logger.add(BASE_DIR / "logs/translation_logs.log", rotation="10 MB")
+
 
 class TranslationService:
     _source_dir = BASE_DIR / 'data' / 'source'
@@ -17,10 +20,12 @@ class TranslationService:
 
     def __init__(self, source_lang_code: SourceLangsCodes):
         self._source_lang_code = source_lang_code
+        logger.info(f"Translation service initialized for source language: {self._source_lang_code}")
 
     @staticmethod
     def _translate_text(text: str, source_file_lang: SourceLangsCodes, target_lang: str) -> str:
         """Translate text from source language to target language using DeepL API."""
+        logger.debug(f"Translating text from {source_file_lang} to {target_lang}")
         url = 'https://api-free.deepl.com/v2/translate'
         params = {
             'auth_key': settings.DEEPL_API_KEY,
@@ -30,24 +35,33 @@ class TranslationService:
         }
         response = requests.post(url, params=params)
         if response.status_code == 200:
-            return response.json()['translations'][0]['text']
+            translated_text = response.json()['translations'][0]['text']
+            logger.debug(f"Translation successful: {translated_text}")
+            return translated_text
         else:
-            return f'Translation failed with status code {response.status_code}'
+            logger.error(f"Translation failed with status code {response.status_code}")
+            return f"Translation failed with status code {response.status_code}"
 
     @staticmethod
     def _load_data(file_path: Path) -> Dict:
         """Load data from a TOML file."""
+        logger.debug(f"Loading data from {file_path}")
         with file_path.open('r', encoding='utf-8') as file:
-            return toml.load(file)
+            data = toml.load(file)
+        logger.debug("Data loaded successfully")
+        return data
 
     @staticmethod
     def _save_data(data: Dict, file_path: Path):
         """Save data to a TOML file."""
+        logger.debug(f"Saving data to {file_path}")
         with file_path.open('w', encoding='utf-8') as toml_file:
             toml.dump(data, toml_file)
+        logger.debug("Data saved successfully")
 
     def translate_and_save_to_toml(self, languages: Dict[str, str]):
         """Load data, translate and save to another TOML file."""
+        logger.info("Starting translation process")
         input_file_path = self._source_dir / 'cleanup.toml'
         output_file_path = self._result_dir / 'translations.toml'
 
@@ -63,6 +77,7 @@ class TranslationService:
                 )
 
         self._save_data(data=translations, file_path=output_file_path)
+        logger.info("Translation process completed")
 
 
 if __name__ == '__main__':
