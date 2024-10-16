ee4eaa9d2b0ace035a4b59ba30cfcfef8e24e8b9
ph4nt0mm <100.ac@mail.ru>
2024-10-16 16:27:14 +0300

    env: add chatgpt api token

--- settings/config.py
+++ settings/config.py
@@ -8,6 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
     DEEPL_API_KEY: str = ''
+    CHAT_GPT_API_KEY: str = ''
 
     class Config:
         env_file = Path(BASE_DIR, 'settings', 'env')
