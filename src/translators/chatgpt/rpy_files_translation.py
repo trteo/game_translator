47d303f98c059e57cbd33b8b798adeff07c04735
ph4nt0mm <100.ac@mail.ru>
2024-10-16 16:53:37 +0300

    fix: rpy realisation description

--- src/translators/chatgpt/rpy_files_translation.py
+++ src/translators/chatgpt/rpy_files_translation.py
@@ -8,20 +8,16 @@ Get source lang
 
 Open rpy file
 Parse file (
-        select context
-        split text on blocks by \n\n
-
-        If starts with \t:
-            1 row is a name
-            other lines are speeches
-
-        If no \t at the beginning :
-            its KAYOSHI thoughts block
+    source text like '\t# * "'
+    result must be placed at '\t * "' after above
+    
+    
+    codes after "translate *" must be replaced to short lang codes
     ) 
 
     unite blocks to batches. Each batch must be ~ 80% max response size
-    translate each batch oon other languages and 
-    save to independent files with lang postfix 
+    translate each batch on other languages and 
+    save to independent file into lang folder
 
 Prompt:
 
